1bb69463261002833f49070538036086c8e728c8
linsonglin <linsonglin@roboeye.ai>
2021-03-15 16:26:59 +0800

    change hello.py

--- hello.py
+++ hello.py
@@ -4,8 +4,11 @@
 # Description   :
 # Author        :
 # Creation Date : 2021-03-15
-# Last Modified : Mon 15 Mar 2021 11:29:08 AM CST
+# Last Modified : Mon 15 Mar 2021 04:26:41 PM CST
 # Created By    : lsl
+import os
 
-
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
 print("hello world!")
+print(username, password)
