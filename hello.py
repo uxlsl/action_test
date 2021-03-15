a9f8a7de44ef6c2046941d9b2d947bda9f896d5d
linsonglin <linsonglin@roboeye.ai>
2021-03-15 11:29:24 +0800

    add hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+# File Name     : hello.py
+# Description   :
+# Author        :
+# Creation Date : 2021-03-15
+# Last Modified : Mon 15 Mar 2021 11:29:08 AM CST
+# Created By    : lsl
+
+
+print("hello world!")
